9eef62d72931b638776df8aaff43246330c84c1f
Swastik Das <swastikdasoff@gmail.com>
2024-01-10 14:58:51 +0530

    New

--- Day 8/functions.py
+++ Day 8/functions.py
@@ -1,7 +1,6 @@
-def greet():
-    print("Hello")
-    print("How Do You Do?")
-    print("Isn't the weather nice today?")
+def greet(name):
+    print(f"Hello {name}!")
+    print(f"How You doing {name}?")
+    print(f"Isn't the weather nice today {name}?")
 
-greet()
 greet("Swastik")
---
+++ Day 8/functions2.py
@@ -0,0 +1,3 @@
+def greet_with(name="Swastik",location="Gotham"):
+    print(f"Hello Master {name}")
+    print(f"Welcome to {location}!")
